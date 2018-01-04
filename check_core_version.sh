322b6823481e77670aa2d090607d8c69b006c7f2
davidmlentz <davidmlentz@gmail.com>
2018-01-04 15:45:45 -0700

    Add comments to some scripts.

--- check_core_version.sh
+++ check_core_version.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# For each site listed in data/sites, grab one page and parse it to find the WordPress core version.
+
 i=1
 while read line; do
 	echo $line
 	wget -qO - $line | grep "wp-embed.min.js" | sed s/"<script type='text\/javascript' src='\(.*\)?ver="/"WP core version: "/g | sed s/"'><\/script>"//g
 	echo "\r\n"
         sleep 1
-done <sites
+done <data/sites
--- check_theme_version.sh
+++ check_theme_version.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# For each site listed in data/sites, grab one page and parse it to find the version of the active WordPress theme.
+
 i=1
 while read line; do
 	echo $line
@@ -7,4 +9,4 @@ while read line; do
 	wget -qO - $line | grep "wp-embed.min.js" | sed s/"<script type='text\/javascript' src='\(.*\)?ver="/"WP core version: "/g | sed s/"'><\/script>"//g
 	echo "\r\n"
         sleep 1
-done <sites
+done <data/sites
